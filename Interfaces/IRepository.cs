a8efe0469450dac8009f04aa9b4cdbb3377919f3
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-12-02 18:56:57 +0300

    added I repostory interface

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Survey.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+
+}
