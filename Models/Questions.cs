5b066e952c42319d333e64ea9765b7da555fc29a
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-12-03 12:10:30 +0300

    questions model

--- Models/Questions.cs
+++ Models/Questions.cs
@@ -1,3 +1,4 @@
+using Survey.Models.common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Survey.Models
 {
-    public class Questions
+    public class Questions : AuditLog
     {
-        
+        public long Id { get; set; }
+        public string QuestionText { get; set; }
+        public List<long> ChoiceIds { get; set; }
+        public bool IsGeneral { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
     }
 }
